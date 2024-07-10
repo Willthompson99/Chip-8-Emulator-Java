0492053f227055383c31e668d10177bd54fe0b63
willthompson99 <will.j.thompson@outlook.com>
2024-07-10 10:21:07 -0500

    Add updated files

--- src/Chip8.java
+++ src/Chip8.java
@@ -1,9 +1,43 @@
-public class Chip8 {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-    private int opcode;
-    private char[] memory;
-    private char[] V;
-    private char I;
-    private char pc;
-    
+public class Chip8 {
+    private byte[] memory = new byte[4096];
+    private byte[] V = new byte[16];
+    private int I;
+    private int pc = 0x200;
+    private int delayTimer;
+    private int soundTimer;
+    private boolean[][] display = new boolean[64][32];
+    private boolean[] keys = new boolean[16];
+
+    public Chip8 () {
+        initialize();
+    }
+
+    private void initialize () {
+        pc = 0x200;
+        I = 0;
+        delayTimer = 0;
+        soundTimer = 0;
+    }
+
+    public void loadProgramFromFile(String filePath) throws IOException {
+        byte[] program = Files.readAllBytes(Paths.get(filePath));
+        System.arraycopy(program, 0, memory, 0x200, program.length);
+    }
+
+    public void emulateCycle() {
+        int opcode = ((memory[pc] << 8) | (memory[pc + 1] & 0xFF));
+        switch (opcode & 0xF000) {
+        case 0xA000:
+            I = opcode & 0x0FFF;
+            pc += 2;
+            break;
+        }
+        if (delayTimer > 0) delayTimer--;
+        if (soundTimer > 0) soundTimer--;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
-public class main {
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Chip8 chip8 = new Chip8();
+        
+        String filePath = "";
+
+        try {
+            chip8.loadProgramFromFile(filePath);
+        } catch (IOException e) {
+            System.err.println("Failed to load CHIP-8 program: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
     
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
 }
