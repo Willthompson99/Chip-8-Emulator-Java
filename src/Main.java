e49431153f24893463a8235f80832f83e353de64
willthompson99 <will.j.thompson@outlook.com>
2024-07-10 10:38:16 -0500

    Added file picker

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.io.*;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 
 
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
         Chip8 chip8 = new Chip8();
         
-        String filePath = "";
+        String filePath = chooseFile(frame);
 
         try {
             chip8.loadProgramFromFile(filePath);
@@ -15,5 +18,15 @@ public class Main {
         }
     }
 
-    
+    private static String chooseFile(JFrame parent) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Select a CHIP-8 Program");
+
+        int userSelection = fileChooser.showOpenDialog(parent);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToOpen = fileChooser.getSelectedFile();
+            return fileToOpen.getAbsolutePath();
+        }
+        return null;
+    }
 }
